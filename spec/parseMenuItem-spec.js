ae3ab8c3e91ca04cbe20cbdbf00c62e21f62ee5d
Pete Taylour <pt9538@my.bristol.ac.uk>
2017-12-26 22:13:02 +0000

    parse element not event

--- spec/parseMenuItem-spec.js
+++ spec/parseMenuItem-spec.js
@@ -3,10 +3,6 @@ import parseMenuItem from '../lib/parseMenuItem'
 
 const PATH = 'a/path'
 
-const eventOfElement = element => ({
-    target: element,
-})
-
 const MENU_HEADER_SPAN = {
     dataset: {
         path: PATH,
@@ -22,14 +18,14 @@ const MENU_HEADER_DIV = {
 
 describe('parseMenuItem', () => {
     it('parses inner span of a header for path', () => {
-        expect(parseMenuItem(eventOfElement(MENU_HEADER_SPAN))).toBe(PATH)
+        expect(parseMenuItem(MENU_HEADER_SPAN)).toBe(PATH)
     })
 
     it('parses menu header div of a header for path', () => {
-        expect(parseMenuItem(eventOfElement(MENU_HEADER_DIV))).toBe(PATH)
+        expect(parseMenuItem(MENU_HEADER_DIV)).toBe(PATH)
     })
 
     it('Returns undefined if no path', () => {
-        expect(parseMenuItem(eventOfElement({}))).toBe(undefined)
+        expect(parseMenuItem({})).toBe(undefined)
     })
 })
